a29a2f1ed5ed84ff3ce4ad1e76ed7fec2bcc6c73
vikbaliyan <122321021+vikbaliyan@users.noreply.github.com>
2023-08-06 18:43:59 -0400

    Add files via upload
    app for streamlit

---
+++ app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
+from PIL import Image
+
+# Load the saved model
+model = load_model(r"C:\Users\user\Downloads\Coid19_final\trained_model.h5")
+
+# Class names (change these based on your classes)
+class_names = ['class_1', 'class_2', 'class_3']
+
+def preprocess_image(image):
+    # Resize and preprocess the image
+    image = image.resize((150, 150))
+    image = np.asarray(image) / 255.0
+    image = np.expand_dims(image, axis=0)
+    return image
+
+def predict(image):
+    processed_image = preprocess_image(image)
+    prediction = model.predict(processed_image)
+    predicted_class = np.argmax(prediction, axis=1)
+    return class_names[predicted_class[0]]
+
+def main():
+    st.title('CNN Image Classifier with Streamlit')
+    st.write('Upload an image and the model will predict the class.')
+
+    uploaded_file = st.file_uploader("Choose an image...", type=['jpg', 'jpeg', 'png'])
+
+    if uploaded_file is not None:
+        # Display the uploaded image
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+
+        # Make predictions
+        prediction = predict(image)
+
+        st.write(f'Prediction: {prediction}')
+
+if __name__ == '__main__':
+    main()
